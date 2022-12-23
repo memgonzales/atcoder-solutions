b235ed2b22d2e50e97b07785bd5a3db779c1a5b8
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2022-12-23 22:22:47 +0800

    Add ABC282 Problem A

---
+++ AtCoder Beginner Contest 282/a.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+void
+solve()
+{
+    int n;
+  	cin >> n;
+  
+  	for (int i = 0; i < n; i++) {
+     	cout << (char) ('A' + i); 
+    }
+}
+
+int
+main()
+{
+  	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+    solve();
+
+ 	return 0; 
+}
