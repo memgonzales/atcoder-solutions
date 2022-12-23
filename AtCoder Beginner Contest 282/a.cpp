e19e1af0154e8b6674eb9708f1e5c18e5cb2c9d9
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2022-12-23 22:29:06 +0800

    Standardize code formatting

--- AtCoder Beginner Contest 282/a.cpp
+++ AtCoder Beginner Contest 282/a.cpp
@@ -2,24 +2,23 @@
 
 using namespace std;
 
-void
-solve()
+void solve()
 {
     int n;
-  	cin >> n;
-  
-  	for (int i = 0; i < n; i++) {
-     	cout << (char) ('A' + i); 
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << (char)('A' + i);
     }
 }
 
-int
-main()
+int main()
 {
-  	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
     solve();
 
- 	return 0; 
+    return 0;
 }
