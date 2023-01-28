d41bed6da3722e5973b1e78db482441ab9af550e
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2023-01-28 22:38:04 +0800

    Sanitize code

--- AtCoder Beginner Contest 287/a.cpp
+++ AtCoder Beginner Contest 287/a.cpp
@@ -4,18 +4,6 @@
 
 using namespace std;
 
-bool isSubsetSum(vector<long long int> set, int n, unsigned long long int sum) {
-    if (sum == 0)
-        return true;
-    if (n == 0)
-        return false;
-
-    if (set[n - 1] > sum)
-        return isSubsetSum(set, n - 1, sum);
-
-    return isSubsetSum(set, n - 1, sum) || isSubsetSum(set, n - 1, sum - set[n - 1]);
-}
-
 void solve() {
     int n;
     cin >> n;
